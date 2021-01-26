8a401f27e13d88f53f227872b8c61c692c525e46
EjMat8 <franzelijahmatugas@gmail.com>
2021-01-26 20:54:05 +0800

    fresh

--- js/Views/cardView.js
+++ js/Views/cardView.js
@@ -1,9 +1,19 @@
 class CardView {
   _data;
-  _parentEl = "";
-  _spanElMyst = document.querySelectorAll(".mystery-name");
+  _parentEl = '';
+  _spanElMyst = document.querySelectorAll('.mystery-name');
 }
+
+//front name
+
 // .forEach(function (el) {
 //     el.innerHTML = "";
 //     el.insertAdjacentText("afterbegin", model.state.mystery.myst);
+//   });
+
+//back name
+
+// document.querySelectorAll('.card__mystery-title').forEach((_, i) => {
+//     document.querySelector(`.card__mystery-title--${i + 1}`).innerHTML =
+//       model.state.mystery.mystName[i];
 //   });
--- js/controller.js
+++ js/controller.js
@@ -1,14 +1,26 @@
 // import { from } from "core-js/fn/array";
-import { DAY } from "./config.js";
-import headerView from "./Views/headerView.js";
-import * as model from "./model.js";
+import { DAY } from './config.js';
+import headerView from './Views/headerView.js';
+import * as model from './model.js';
 
-import "core-js/stable";
-import "regenerator-runtime/runtime";
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
 const controlINIT = function () {
   model.loadMystery(DAY);
   headerView.renderHeader(model.state);
 };
-
 controlINIT();
+
+// document.querySelectorAll('.card__side--front').forEach(function (_, i) {
+//   const fileName = `img/url(${model.state.mystery.myst
+//     .split(' ')[0]
+//     .toLowerCase()}-${i + 1}).jpg`;
+
+//   document.querySelector(
+//     `.card__side--front--${i + 1}`
+//   ).style.backgroundImage = `url('${images[fileName]}')`;
+// });
+// // url('img/url(${model.state.mystery.myst
+// //   .split(" ")[0]
+// //   .toLowerCase()}-${i + 1})')
